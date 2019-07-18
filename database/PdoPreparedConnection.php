89f99a812695b32b18f578a954fdd4512dd09eb8
Florian Häußinger <floderflo@gmx.de>
2019-07-18 07:34:25 +0200

    Weitere Pfadanpassungen

--- database/PdoPreparedConnection.php
+++ database/PdoPreparedConnection.php
@@ -3,7 +3,7 @@
 include_once __DIR__ . "/IfDatabseConnection.php";
 include_once __DIR__ . "/AbstractSqlConnection.php";
 include_once __DIR__ . "/SqlQueryFactory.php";
-include_once __DIR__ . '/../../bs-php-utils/utils.php';
+include_once __DIR__ . '/../../../bensteffen/bs-php-utils/utils.php';
 
 class PdoPreparedConnection extends AbstractSqlConnection implements IfDatabseConnection {
     private $pdo = null;
