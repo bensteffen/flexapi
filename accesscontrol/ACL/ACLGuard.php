d7928262c66a78b2a83efb88d283bb5c1c762f94
Florian Häußinger <floderflo@gmx.de>
2019-10-22 17:52:23 +0200

    set distinct fixed to false

--- accesscontrol/ACL/ACLGuard.php
+++ accesscontrol/ACL/ACLGuard.php
@@ -314,7 +314,7 @@ class ACLGuard extends Guard {
 
     public function readPermitted($connection, $entity, $filter, $selection, $sort, $onlyOwn = false) {
         $filter = $this->addPermissionFilter('R', $entity, $filter, $onlyOwn);
-        return $connection->readFromDataBase($entity, $filter, $selection, $onlyOwn, $sort);
+        return $connection->readFromDataBase($entity, $filter, $selection, false, $sort);
     }
 
     public function updatePermitted($connection, $entity, $filter, $data) {
