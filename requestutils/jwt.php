80a6a7c6c8573967380c0b237d020c472b94233d
Florian Häußinger <floderflo@gmx.de>
2020-07-26 13:03:29 +0200

    polyfill for getallheaders

--- requestutils/jwt.php
+++ requestutils/jwt.php
@@ -2,6 +2,17 @@
 
 const JWT_HEADER_NAME='Access-Control-Allow-Credentials';
 function getJWT() {
+    if (!function_exists('getallheaders')) {
+        function getallheaders() {
+            $headers = [];
+            foreach ($_SERVER as $name => $value) {
+                if (substr($name, 0, 5) == 'HTTP_') {
+                    $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+                }
+            }
+            return $headers;
+        }
+    }
     $http_header = getallheaders();
     $jwt = null;
     if (array_key_exists(JWT_HEADER_NAME, $http_header)) {
